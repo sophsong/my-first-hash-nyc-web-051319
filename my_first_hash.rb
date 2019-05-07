2ad7896d960effb259bc60daa80df560783d1f2a
Sophia Song <ososanne@gmail.com>
2019-05-07 02:56:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.	 your_hash = {"name" => "Sara", "age" => 31, "eye color" => "brown"
+your_hash = {"name" => "Sophia"}
 
-  }
+
 
  end	end
 
@@ -9,44 +8,42 @@ def my_hash
 
 
  def shipping_manifest	def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash	 the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3
-  # fill that hash with key/value pairs that describe the following information: 	 }
+  # set a variable called `the_manifest`, equal to a hash	  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  # fill that hash with key/value pairs that describe the following information: 	
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 	
+
 
 
  end	end
 
 
- def retrieval	def retrieval
-@@ -21,7 +17,7 @@ def retrieval
+ @@ -20,7 +16,7 @@ def retrieval
+                  "oil paintings" => 3	                  "oil paintings" => 3
                  }	                 }
 
 
-   # Look up the value of the "oil paintings" key in the shipping_manifest hash below	  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
- shipping_manifest["oil paintings"]
-end	end
+   # Look up the value of the "oil paintings" key in the shipping_manifest hash below	  shipping_manifest["oil paintings"]
 
 
- def adding	def adding
-@@ -32,12 +28,18 @@ def adding
+ end	end
+
+
+ @@ -31,13 +27,14 @@ def adding
+                  "oil paintings" => 3	                  "oil paintings" => 3
                  }	                 }
   # add 2 muskets to the shipping_manifest hash below	  # add 2 muskets to the shipping_manifest hash below
 
+   shipping_manifest["muskets"] = 2
 
 
-   shipping_manifest["muskets"]= 2
 
 
    # add 4 gun powder to the shipping_manifest hash below	  # add 4 gun powder to the shipping_manifest hash below
 
+   shipping_manifest["gun powder"] = 4
 
 
-   shipping_manifest["gun powder"]= 4
 
 
    # return the shipping_manifest hash below	  # return the shipping_manifest hash below
-
-
-   shipping_manifest 
-end	end
+  shipping_manifest
