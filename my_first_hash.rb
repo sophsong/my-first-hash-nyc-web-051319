8c3e338076ec3f4cea53fd152ee87d52fa033d6f
Sophia Song <ososanne@gmail.com>
2019-05-07 02:55:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ def shipping_manifest
 the manifest={
   "whale bone corsets"=5,
   "porcelain vases"=2,
-  "oil paintings"=3
+  "oil paintings"=3,
   
 }
 
@@ -32,7 +32,7 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
                  }
   # add 2 muskets to the shipping_manifest hash below
 
@@ -43,8 +43,8 @@ def adding
 
 
   # return the shipping_manifest hash below
-  shipping_manifest["muskets"]=2
-shipping_manifest["gun powder"]=4
+  shipping_manifest["muskets"]= 2
+shipping_manifest["gun powder"]= 4
 
 end
 shipping_manifest
