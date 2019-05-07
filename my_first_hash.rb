1194c2e3dec1700fff0f1e4965d31de16ef8a5e2
Sophia Song <ososanne@gmail.com>
2019-05-07 02:57:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,49 +1,44 @@
-your_hash = {"name" => "Sophia"}
+def my_hash
+  
+  your_hash = {"name" => "Sophia"}
+end
 
 
+def shipping_manifest
+  
 
- end	end
+  the_manifest = {
+      "whale bone corsets" => 5, 
+      "porcelain vases" => 2, 
+      "oil paintings" => 3
+    }
+end
 
-
-
-
- def shipping_manifest	def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash	  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  # fill that hash with key/value pairs that describe the following information: 	
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 	
-
-
-
- end	end
-
-
- @@ -20,7 +16,7 @@ def retrieval
-                  "oil paintings" => 3	                  "oil paintings" => 3
-                 }	                 }
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
 
 
-   # Look up the value of the "oil paintings" key in the shipping_manifest hash below	  shipping_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
+end
+
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+ 
+  shipping_manifest["muskets"] = 2
 
 
- end	end
+  
+  shipping_manifest["gun powder"] = 4
 
 
- @@ -31,13 +27,14 @@ def adding
-                  "oil paintings" => 3	                  "oil paintings" => 3
-                 }	                 }
-  # add 2 muskets to the shipping_manifest hash below	  # add 2 muskets to the shipping_manifest hash below
-
-   shipping_manifest["muskets"] = 2
-
-
-
-
-   # add 4 gun powder to the shipping_manifest hash below	  # add 4 gun powder to the shipping_manifest hash below
-
-   shipping_manifest["gun powder"] = 4
-
-
-
-
-   # return the shipping_manifest hash below	  # return the shipping_manifest hash below
+ 
   shipping_manifest
+end
